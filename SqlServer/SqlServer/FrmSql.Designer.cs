355d1e87e4b022f08bfe20308b42d5b6cb9ad0b2
xiaozhi242426 <2033643295@qq.com>
2024-07-26 15:22:57 +0800

    Add files via upload

---
+++ SqlServer/SqlServer/FrmSql.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SqlServer
+{
+    partial class FrmSql
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSqlStr = new System.Windows.Forms.TextBox();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnFindStr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSqlStr
+            // 
+            this.txtSqlStr.Font = new System.Drawing.Font("宋体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtSqlStr.Location = new System.Drawing.Point(91, 74);
+            this.txtSqlStr.Name = "txtSqlStr";
+            this.txtSqlStr.Size = new System.Drawing.Size(1171, 58);
+            this.txtSqlStr.TabIndex = 0;
+            // 
+            // btnCon
+            // 
+            this.btnCon.Font = new System.Drawing.Font("宋体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCon.Location = new System.Drawing.Point(614, 162);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(303, 63);
+            this.btnCon.TabIndex = 1;
+            this.btnCon.Text = "连接";
+            this.btnCon.UseVisualStyleBackColor = true;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Font = new System.Drawing.Font("宋体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtInfo.Location = new System.Drawing.Point(91, 260);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(1171, 413);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Font = new System.Drawing.Font("宋体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnClean.Location = new System.Drawing.Point(959, 162);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(303, 63);
+            this.btnClean.TabIndex = 3;
+            this.btnClean.Text = "清空";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // btnFindStr
+            // 
+            this.btnFindStr.Font = new System.Drawing.Font("宋体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFindStr.Location = new System.Drawing.Point(91, 162);
+            this.btnFindStr.Name = "btnFindStr";
+            this.btnFindStr.Size = new System.Drawing.Size(303, 63);
+            this.btnFindStr.TabIndex = 4;
+            this.btnFindStr.Text = "获取字符串";
+            this.btnFindStr.UseVisualStyleBackColor = true;
+            this.btnFindStr.Click += new System.EventHandler(this.btnFindStr_Click);
+            // 
+            // FrmSql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 803);
+            this.Controls.Add(this.btnFindStr);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnCon);
+            this.Controls.Add(this.txtSqlStr);
+            this.Name = "FrmSql";
+            this.Text = "SqlServer连接测试";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSqlStr;
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnFindStr;
+    }
+}
+
---
+++ SqlServer/SqlServer/FrmSql.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlServer
+{
+    public partial class FrmSql : Form
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["xiaozhi"].ConnectionString;
+
+        public FrmSql()
+        {
+            InitializeComponent();
+        }
+
+        public void ConnectToSqlServer()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    txtInfo.Text += $"SELECT TOP 1 * FROM TB_USER WHERE USER_NAME = ''hzkk'' AND USER_PWD = ''hzkk'' AND USER_VALID = ''Y''" + "\r\n";
+
+                    string query = "SELECT TOP 1 * FROM TB_USER WHERE USER_NAME = 'hzkk' AND USER_PWD = 'hzkk' AND USER_VALID = 'Y'";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // 输出整条数据
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    txtInfo.Text += reader[i].ToString(); // 以空格分隔每个字段
+                                }
+                                txtInfo.Text += "\r\n"; // 换行显示下一行数据
+                            }
+                        }
+                    }
+
+                    txtInfo.Text += "connecting to SQL Server: " + "\r\n";
+
+                }
+                catch (Exception ex)
+                {
+                    txtInfo.Text += "Error connecting to SQL Server: " + ex.Message + "\r\n";
+                }
+            }
+        }
+
+        private void btnFindStr_Click(object sender, EventArgs e)
+        {
+            txtSqlStr.Text = connectionString;
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtInfo.Text = "";
+        }
+
+        private void btnCon_Click(object sender, EventArgs e)
+        {
+            ConnectToSqlServer();
+        }
+    }
+}
